b1657136a0ad42579ffcab9f37d7fcab9a71d4da
devilbox <asdkx7@gmail.com>
2023-06-04 16:07:15 -0600

    Initial commit

---
+++ todo-list-api/todo-list-api/src/todo-list-api/Startup.cs
@@ -0,0 +1,63 @@
+﻿
+using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace ToDoList
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+        var connectionString = Configuration.GetConnectionString("DefaultConnection");
+
+        if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new InvalidOperationException("DefaultConnection setting not defined in appsettings.json");
+            }
+
+            services.AddDbContext<TodoDbContext>(x => x.UseSqlServer(connectionString,
+                builder =>
+                {
+                    builder.CommandTimeout(800);
+                    builder.EnableRetryOnFailure(
+                        maxRetryCount: 5,
+                        maxRetryDelay: TimeSpan.FromSeconds(60),
+                        errorNumbersToAdd: null);
+
+                }));
+
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Welcome to running ASP.NET Core on AWS Lambda");
+                });
+            });
+        }
+    }
+}
---
+++ todo-list-api/todo-list-api/src/todo-list-api/TodoDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+namespace ToDoList
+{
+    public class TodoDbContext : DbContext
+    {
+
+    }
+}
